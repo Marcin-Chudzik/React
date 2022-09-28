806e3e3de19ab3f65033653859d5514a749ca19f
Marcin_Chudzik <marcinchudzik.juniordev@gmail.com>
2022-09-28 04:31:42 +0200

    Initialize new project, heroApi

---
+++ heroapi/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <nav></nav>
+    <main>
+      
+    </main>
+    <footer></footer>
+    </>
+  );
+};
+
+export default App;
